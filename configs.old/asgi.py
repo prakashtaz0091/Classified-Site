61b419e9c8d870902048c49b75862d14adf61d2b
bishal89 <bishalmurmu150@gmail.com>
2024-08-09 13:14:10 +0545

    added updated code on wsgi asgi

--- configs.old/asgi.py
+++ configs.old/asgi.py
@@ -7,10 +7,27 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
-import os
+# import os
 
+# from django.core.asgi import get_asgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'configs.settings.settings')
+
+# application = get_asgi_application()
+
+import os
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'configs.settings.settings')
+# Fetch the DJANGO_ENV environment variable, defaulting to "production" if not set
+environment = os.environ.get("DJANGO_ENV", "production")
 
+# Set the DJANGO_SETTINGS_MODULE environment variable based on the DJANGO_ENV
+if environment == "production":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "configs.settings.production")
+elif environment == "development":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "configs.settings.development")
+else:
+    raise ValueError(f"Unknown DJANGO_ENV: {environment}")
+
+# Initialize the ASGI application
 application = get_asgi_application()
