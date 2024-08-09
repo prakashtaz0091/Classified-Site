4025065d387f0bd0478f887c6141c87e7fd8881f
bishal89 <bishalmurmu150@gmail.com>
2024-08-09 13:27:09 +0545

    updated

--- config/asgi.py
+++ config/asgi.py
@@ -1,8 +1,16 @@
 import os
-
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE",
-                      f"config.settings.{environment}")
+# Fetch the DJANGO_ENV environment variable, defaulting to "production" if not set
+environment = os.environ.get("DJANGO_ENV", "production")
 
+# Set the DJANGO_SETTINGS_MODULE environment variable based on the DJANGO_ENV
+if environment == "production":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "configs.settings.production")
+elif environment == "development":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "configs.settings.development")
+else:
+    raise ValueError(f"Unknown DJANGO_ENV: {environment}")
+
+# Initialize the ASGI application
 application = get_asgi_application()
