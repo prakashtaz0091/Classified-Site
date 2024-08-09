f353537231b05157399672cc9ec0335f1de4402b
bishal89 <bishalmurmu150@gmail.com>
2024-08-09 12:04:10 +0545

    updated

--- common/common.py
+++ common/common.py
@@ -127,3 +127,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# WSGI_APPLICATION = 'configs.wsgi.application'
+
+# # ASGI application
+# ASGI_APPLICATION = 'configs.asgi.application'
