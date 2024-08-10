4b78eccac7265955865b3e680903f2842a3327a8
bishal89 <bishalmurmu150@gmail.com>
2024-08-10 12:13:28 +0545

    solved the static issue in admin pannel

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Account
+# from  django.contrib.auth.admin import UserAdmin
 # Register your models here.
+
+class Account_admin(admin.ModelAdmin):
+    list_display=('email','last_login','is_active','is_superadmin')
+    ordering=('-date_joined',)
+    readonly_fields=('last_login','date_joined')
+    filter_horizontal=()
+    list_filter=()
+    fieldsets=()
+    
+    list_display_links=('email','last_login','is_active','is_superadmin')
+admin.site.register(Account,Account_admin)
