9c9900aa9a424e4e20e063412499f3fafa4b0ee1
bishal89 <bishalmurmu150@gmail.com>
2024-08-12 12:02:04 +0545

    added paginations

--- apps/category/urls.py
+++ apps/category/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     # path('category/', views.category_view, name='category'),
     path('<slug:slug>/', views.listing_view, name='listing'),
+    # path('api/products/', views.get_products, name='get_products'),
 ]
--- apps/category/views.py
+++ apps/category/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from apps.category.models import Category
 from apps.store.models import Product
+from django.http import JsonResponse
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404
+
 # Create your views here.
 # def category_view(request):
 #     categories = Category.objects.prefetch_related('sub_categories').all()
@@ -19,7 +23,22 @@ from apps.store.models import Product
 def listing_view(request,slug):
     category=Category.objects.get(slug=slug)
     products=Product.objects.filter(category=category)
-    context={
-        'products':products
+    count=products.count()
+    
+    
+    # for pagination 
+    
+    
+    paginator = Paginator(products, 2)  
+    page_number = request.GET.get('page')  
+    page_obj = paginator.get_page(page_number)  
+
+    context = {
+        'products': page_obj, 
+        'count': paginator.count,
+        'category': category,  
+        'page_obj': page_obj, 
     }
+   
     return render(request,'listing/listing-sidebar.html',context)
+
