5da09683caa241abb58e51b9ee583d0a048cb170
sandeshsitaula <navaltajohan@gmail.com>
2024-09-06 14:21:04 +0545

    urls and configs fixed

--- apps/business_listing/apps.py
+++ apps/business_listing/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class BusinessListingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'business_listing'
+    name = 'apps.business_listing'
--- apps/business_listing/urls.py
+++ apps/business_listing/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('/',views.list_business_listings,name='list business listings'),
+    path('',views.list_business_listings,name='list business listings'),
 ]
