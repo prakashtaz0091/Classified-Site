3a2c80de754d68e9b0c421b2fc393664918582c0
sandeshsitaula <navaltajohan@gmail.com>
2024-09-22 23:41:11 +0545

    vechiles category ui completed

--- apps/vechiles/views.py
+++ apps/vechiles/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 
+from apps.store.models import Product
+
 #Property landing page
 def landing_page(request):
     try:
-        return render(request,'vechiles/landing.html')
+        featured_products=Product.objects.all()[:4]
+        context={
+            'featured_products':featured_products
+        }
+        print(context)
+        return render(request,'vechiles/landing.html',context)
     except Exception as e:
         print(e)
         # will later return to some 400 page
