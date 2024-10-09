bfc7ee45b81732d0a922497ab2a010a46831609e
bishal89 <bishalmurmu150@gmail.com>
2024-10-09 08:15:31 +0545

    added the number of listing

--- apps/property/views.py
+++ apps/property/views.py
@@ -2,39 +2,43 @@ from django.shortcuts import render
 from apps.category.models import Category
 from apps.store.models import Product
 from django.core.paginator import Paginator
-
+from django.db.models import Count
 
 #Property landing page
 def landing_page(request):
     try:
+        # Fetch featured products, limiting to the 4 most recent
         featured_products = Product.objects.all().order_by('-id')[:4]
-        all_categories = Category.objects.filter(parent_id=12,status='ACTIVE')        
+        
+        # Fetch categories and annotate them with the count of related products
+        all_categories = Category.objects.filter(parent_id=12, status='ACTIVE').annotate(product_count=Count('products'))
+
         latest_products = []
         all_products = (
             Product.objects.select_related()
-            .filter(is_available=True,is_approved=True)
+            .filter(is_available=True, is_approved=True)
             .order_by("-created_date")
         )
-            # Get the latest 5 products for each category
+        
+        # Get the latest 5 products for each category
         for category in all_categories:
-            products = all_products.filter(category=category,is_approved=True,is_available=True)[:4]
+            products = all_products.filter(category=category, is_approved=True, is_available=True)[:4]
             latest_product = {"products": products, "category": category}
             latest_products.append(latest_product)
-            
 
-        context={
-            'featured_products':featured_products,
-            'latest_products':latest_products,
-            'subcategories':all_categories
+        # Context to pass to the template
+        context = {
+            'featured_products': featured_products,
+            'latest_products': latest_products,
+            'subcategories': all_categories,  # This will include the annotated product count
         }
-        return render(request,'properties/landing.html',context)
+
+        return render(request, 'properties/landing.html', context)
 
     except Exception as e:
-        print(e)
-        # will later return to some 400 page
-        return e
-
-
+        # Handle any potential errors here, such as logging them
+        print(f"Error in landing_page: {e}")
+        return render(request, 'error_page.html')
 
 
 
