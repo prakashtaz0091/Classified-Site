7e3ac544d75e7018e019dcdf05a56f5ecfa669cc
sandeshsitaula <navaltajohan@gmail.com>
2024-10-11 11:14:42 +0545

    header done

--- apps/job/urls.py
+++ apps/job/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path("",views.landing_page,name='landing_page'),
+   path("",views.landing_page,name='job_landing_page'),
    path("job_category/",views.job_category,name='job_category'),
    path("all_jobs/",views.view_all_jobs,name='all_jobs'),
    path("search_jobs/",views.search_job,name='search_job'),
